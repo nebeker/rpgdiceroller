06feb8bd36d455aa50d47fba0bc42fa90096773f
Nuno Nebeker <nunonebeker@gmail.com>
2020-10-05 18:00:15 +0100

    Improved imput checks.

--- rpgdiceroller.py
+++ rpgdiceroller.py
@@ -1,15 +1,15 @@
 import random
 
 def rolld(faces):
-	"Rolls a d-faces"
-	if faces < 4:
-		raise Exception("Die must have at least 4 faces.")
+	"Rolls a d-faces die"
+	if faces not in [4, 6, 8, 10, 12, 20]:
+		raise Exception("Must be a regular sided die.")
 	return random.randint(1, faces)
 
 def rollnd(n, faces):
-	"Rolls n face dice"
-	if n < 1:
-		raise Exception("Must roll at least one die")
+	"Rolls n faces dice"
+	if not isinstance(n, int) or n < 1:
+		raise Exception("Must roll at least one die.")
 	rolls = []
 	for i in range(n):
 		rolls.append(rolld(faces))	
