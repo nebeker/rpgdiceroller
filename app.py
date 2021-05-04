7a6aa7a2752ce9af967f3f45fff285f1e80a5ccf
Nuno Nebeker <nunonebeker@gmail.com>
2021-05-04 21:39:27 +0100

    Handle empty faces field and retained checkboxs.

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
     result = 0
 
     if request.method == 'POST':
-        faces = int(request.form['faces'])
+        if request.form['faces']:
+            faces = int(request.form['faces'])
+        else:
+            faces = 20
         if request.form['mod']:
             mod = int(request.form['mod'])
         else:
@@ -18,20 +22,20 @@ def index():
         disadvantage = request.form.get('disadvantage')
 
         if advantage and not disadvantage:
-            if (mod!=0):
+            if (mod != 0):
                 result = rolladmod(faces, mod)
             else:
                 result = rollad(faces)
         elif disadvantage and not advantage:
-            if (mod!=0):
+            if (mod != 0):
                 result = rolldismod(faces, mod)
             else:
                 result = rolldis(faces)
-        elif (mod!=0):
+        elif (mod != 0):
             result = rollmod(faces, mod)
         else:
             result = rolld(faces)
 
         redirect(request.referrer)
 
-    return render_template('index.html', result = result)
+    return render_template('index.html', result=result, advantage=advantage, disadvantage=disadvantage)
